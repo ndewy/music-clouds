9a60b5ee93ed3fd60b208ac63375daa402d81bb0
Niklas Dewally <Niklas.Dewally@gmail.com>
2021-02-26 22:14:46 +0000

    Added GUI
    * Added GUI
    * Reformatted code
    * Implemented Song and Album modes
    * Gitignore update
    * Added save menu

--- wordle_cloud.py
+++ wordle_cloud.py
@@ -1,24 +1,144 @@
-import pylast
-import cairo
-import tkinter
-from math import ceil,sin,cos,pi
-import random
+import io
 import os
+import random
+import threading
+import tkinter as tk
+import tkinter.filedialog as filedialog
+import tkinter.messagebox as messagebox
+from math import ceil, cos, pi, sin
+
+import cairo
+import pylast
+from PIL import Image, ImageTk
 
 
+class ImageWindow(tk.Toplevel):
+    def __init__(self, image, master=None):
+        super().__init__(master)
+        self.topmenu = tk.Menu(self)
+        self["menu"] = self.topmenu
+        self.filemenu = tk.Menu(self.topmenu,tearoff=0)
+        self.topmenu.add_cascade(label="File",menu=self.filemenu)
+        self.filemenu.add_command(label="Save As",command= self._save)
 
-#region Parameters
-WIDTH, HEIGHT = 2000, 1000 
-LIMIT = 30 
-PERIOD = pylast.PERIOD_1MONTH
+        self.resizable(width=False, height=False)
+        self.image = image
+        thumbnail = image.copy()
+        thumbnail.thumbnail((500, 500))
+        self.tkimage = ImageTk.PhotoImage(thumbnail)
+        self.master = master
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label = tk.Label(self, image=self.tkimage)
+        # self.label.bind('<Configure>', self.resize)
+        self.label.pack()
+
+    def report_callback_exception(self, *args):
+        self.destroy()
+
+    def resize(self, event):
+        new_width = self.winfo_width()
+        new_height = self.winfo_height()
+        self.tkimage = ImageTk.PhotoImage(self.image.resize((new_width, new_height)))
+        self.label.config(image=self.tkimage)
+        self.label.image = self.tkimage
+
+    def _save(self):
+        f = filedialog.asksaveasfile(mode="wb",parent=self,title="Save Picture",filetypes=[("PNG","*.png")],defaultextension='.png')
+
+        self.image.save(f,format="png")
+        f.close()
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title("LastFM")
+        self.pack()
+        self.create_widgets()
+
+        self.master.resizable(width=False, height=False)
+
+    def create_widgets(self):
+        self.options = {}
+
+        self.options["time"] = tk.StringVar()
+        time_label = tk.Label(self, text="Time period:")
+        time_menu = tk.OptionMenu(self, self.options["time"], *TIME_PERIODS.keys())
+        self.options["time"].set("All Time")
+
+        self.options["type"] = tk.StringVar()
+        type_label = tk.Label(self, text="Select Graph type:")
+        type_menu = tk.OptionMenu(self, self.options["type"], *TYPE_FUNCTIONS.keys())
+        self.options["type"].set("Artist")
+
+        self.options["limit"] = tk.StringVar()
+        self.validate = self.master.register(self.integer_validate)
+        limit_input = tk.Entry(
+            self,
+            textvariable=self.options["limit"],
+            validatecommand=(self.validate, "%S"),
+            validate="key",
+        )
+        limit_input.insert(0, "20")
+        limit_label = tk.Label(self, text="Limit:")
+
+        self.options["user"] = tk.StringVar()
+        users_label = tk.Label(self, text="Username:")
+        self.options["user"].set("ndewy")
+        users_input = tk.Entry(self, textvariable=self.options["user"])
+
+        create = tk.Button(self)
+        create["text"] = "Create"
+        create["command"] = self.generate
+
+        time_label.grid(column=0, row=0)
+        time_menu.grid(column=1, row=0)
+        type_label.grid(column=0, row=1)
+        type_menu.grid(column=1, row=1)
+        limit_label.grid(column=0, row=2)
+        limit_input.grid(column=1, row=2)
+        users_label.grid(column=0, row=3)
+        users_input.grid(column=1, row=3)
+        create.grid(column=2, row=4)
+
+    def generate(self):
+        params = {
+            "time": TIME_PERIODS[self.options["time"].get()],
+            "user": self.options["user"].get(),
+            "type": TYPE_FUNCTIONS[self.options["type"].get()],
+        }
+
+        if self.options["limit"].get() == "":
+            params["limit"] = 10
+        else:
+            params["limit"] = int(self.options["limit"].get())
+        t = threading.Thread(
+            target=generate, args=(self.on_generated, self.on_generating_fail, params)
+        )
+        t.start()
+
+    def on_generated(self, image_path):
+        ImageWindow(image_path, master=self)
+
+    def on_generating_fail(self, msg):
+        messagebox.showerror("Generation Failed", str(msg))
+
+    def integer_validate(self, char):
+        return char.isdigit()
+
+
+# region Parameters
+WIDTH, HEIGHT = 2000, 1000
 MAX_FONT_SIZE = 150
 MIN_FONT_SIZE = 30
 BASE = 1.1
-STEPSIZE = pi/200
+STEPSIZE = pi / 200
 CURVE_MULTIPLIER = 1
 
-#endregion
-#region LastFM stuff
+# endregion
+# region LastFM stuff
 API_KEY = os.environ.get("LASTFM_API_KEY")
 API_SECRET = os.environ.get("LASTFM_API_SECRET")
 
@@ -26,99 +146,162 @@ network = pylast.LastFMNetwork(
     api_key=API_KEY,
     api_secret=API_SECRET,
 )
-#endregion 
+# endregion
 
-def get_spiral_coords(theta):
-    x = ceil(CURVE_MULTIPLIER *theta * cos(theta)) + WIDTH / 2
-    y = ceil(CURVE_MULTIPLIER*theta * sin(theta)) + HEIGHT / 2
-    return x,y
 
-def choose_colour():
+def _get_spiral_coords(theta):
+    x = ceil(CURVE_MULTIPLIER * theta * cos(theta)) + WIDTH / 2
+    y = ceil(CURVE_MULTIPLIER * theta * sin(theta)) + HEIGHT / 2
+    return x, y
+
+
+def _choose_colour():
     # All colours must be atleast 0.5 to stop looking too dark
     while True:
-        r = random.uniform(0.4,1)
-        g = random.uniform(0.4,1)
-        b = random.uniform(0.4,1)    
-        # However, we dont want to be pure white, or near to it, so we max the sum to 2 (out of a maximum of 3).
-        if (r+g+b) < 2:
+        r = random.uniform(0.4, 1)
+        g = random.uniform(0.4, 1)
+        b = random.uniform(0.4, 1)
+        # However, we dont want to be pure white, or near to it, so we max the
+        # sum to 2 (out of a maximum of 3).
+        if (r + g + b) < 2:
             break
-    return r,g,b
+    return r, g, b
 
-#region artists processing
-artists_raw = network.get_user("ndewy").get_top_artists(period=PERIOD,limit=LIMIT)
-#Turn this into an array of artists sorted by plays
-artists = []
-for artist in artists_raw:
-    artists.append(str(artist.item))
 
-# Make font sizes follow negative exponential curve with base BASE
-artist_sizes =[]
+def _get_albums(user, period, limit):
+    """ Returns parsed list of album names """
 
-for i,artist in enumerate(artists):
-        size = ceil(MAX_FONT_SIZE/(BASE**i))
-        if size<=MIN_FONT_SIZE:
+    albums_raw = network.get_user(user).get_top_albums(period=period, limit=limit)
+    albums = [album.item.get_title() for album in albums_raw]
+    return albums
+
+
+def _get_artists(user, period, limit):
+    """ Returns parsed list of artist names """
+
+    artists_raw = network.get_user(user).get_top_artists(period=period, limit=limit)
+    artists = [artist.item.get_name() for artist in artists_raw]
+    return artists
+
+
+def _get_tracks(user, period, limit):
+    """ Returns parsed list of track names """
+
+    tracks_raw = network.get_user(user).get_top_tracks(period=period, limit=limit)
+    tracks = [track.item.get_name() for track in tracks_raw]
+    return tracks
+
+
+def generate(callback_success, callback_fail, parameters):
+    """ Generates wordle"""
+    try:
+        items = parameters["type"](
+            parameters["user"], parameters["time"], parameters["limit"]
+        )
+    except pylast.WSError as e:
+        callback_fail(e)
+        return
+
+    # Make font sizes follow negative exponential curve with base BASE
+    font_sizes = []
+    for i, item in enumerate(items):
+        size = ceil(MAX_FONT_SIZE / (BASE ** i))
+        if size <= MIN_FONT_SIZE:
             size = MIN_FONT_SIZE
-        artist_sizes.append((size,artist))
-#endregion
+        font_sizes.append((size, item))
 
-#region Cairo Image processing
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
-ctx = cairo.Context(surface)
-ctx.rectangle(0, 0, WIDTH, HEIGHT)
-ctx.set_source_rgb(1, 1, 1)
-ctx.fill()
-ctx.set_source_rgb(0, 0, 0)
-ctx.set_font_size(200)
-ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, 
-    cairo.FONT_WEIGHT_NORMAL)
+    # region Cairo Image processing
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+    ctx = cairo.Context(surface)
+    ctx.rectangle(0, 0, WIDTH, HEIGHT)
+    ctx.set_source_rgb(1, 1, 1)
+    ctx.fill()
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.set_font_size(200)
+    ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 
-#Generate text extents
-text_extents = []
-for font_size,artist in artist_sizes:
-    ctx.set_font_size(font_size)
-    text_extents.append(ctx.text_extents(artist))
+    # Generate text extents
+    text_extents = []
+    for font_size, item in font_sizes:
+        ctx.set_font_size(font_size)
+        text_extents.append(ctx.text_extents(item))
 
-rectangles= []
-for i,(font_size,artist) in enumerate(artist_sizes):
-    theta = random.randint(0,300) # Choose a random start position
-    print(artist + "   " + str(font_size))
-    extent = text_extents[i]
-    
-    good_position = False # you cannot have a good position until you are colliding with nothing.
-    while not good_position:
-        #Collision Detection
-        ax1,ay2 = get_spiral_coords(theta) #We get the bottom left corner, so ay2 as y goes down here
-        ax2 = ax1 + extent.width
-        ay1 = ay2 - extent.height
-        #First item will have no rectangles to compare against.
-        if i==0:
-            good_position=True
-        for j in range(0,len(rectangles)):
-            bx1 = rectangles[j][0]
-            bx2 = rectangles[j][1]
-            by1 = rectangles[j][2]
-            by2 = rectangles[j][3]
+    rectangles = []
+    for i, (font_size, item) in enumerate(font_sizes):
+        theta = random.randint(0, 300)  # Choose a random start position
+        print(item + "   " + str(font_size))
+        extent = text_extents[i]
 
-            x_colliding = ( bx1 <= ax1 and ax1 <= bx2) or (bx1 <= ax2 and ax2 <= bx2) or ( ax1 <= bx1 and bx1 <= ax2) or (ax1 <= bx2 and bx2 <= ax2)
-            y_colliding = (by1 <= ay1 and ay1 <= by2) or (by1 <= ay2 and ay2 <= by2) or (ay1 <= by1 and by1 <= ay2) or (ay1 <= by2 and by2 <= ay2)
+        good_position = False  # you cannot have a good position until you are
+        # colliding with nothing.
+        while not good_position:
+            # Collision Detection
+            ax1, ay2 = _get_spiral_coords(theta)  # We get the bottom left
+            # corner, so ay2 as y goes down here
+            ax2 = ax1 + extent.width
+            ay1 = ay2 - extent.height
+            # First item will have no rectangles to compare against.
+            if i == 0:
+                good_position = True
+            for j in range(0, len(rectangles)):
+                bx1 = rectangles[j][0]
+                bx2 = rectangles[j][1]
+                by1 = rectangles[j][2]
+                by2 = rectangles[j][3]
 
-            if x_colliding and y_colliding:
-                good_position = False
-                break
-            good_position = True
+                x_colliding = (
+                    (bx1 <= ax1 and ax1 <= bx2)
+                    or (bx1 <= ax2 and ax2 <= bx2)
+                    or (ax1 <= bx1 and bx1 <= ax2)
+                    or (ax1 <= bx2 and bx2 <= ax2)
+                )
+                y_colliding = (
+                    (by1 <= ay1 and ay1 <= by2)
+                    or (by1 <= ay2 and ay2 <= by2)
+                    or (ay1 <= by1 and by1 <= ay2)
+                    or (ay1 <= by2 and by2 <= ay2)
+                )
 
-        #Move a bit
-        if not good_position:
-            theta += STEPSIZE
+                if x_colliding and y_colliding:
+                    good_position = False
+                    break
+                good_position = True
 
-    #Draw Text
-    x,y = get_spiral_coords(theta)
-    ctx.move_to(x,y)
-    ctx.set_font_size(font_size)
-    r,g,b = choose_colour()
-    ctx.set_source_rgb(r, g, b)
-    ctx.show_text(artist)
-    #Add rectangle to rectangles
-    rectangles.append((x,x+extent.width,y-extent.height,y))
+            # Move a bit
+            if not good_position:
+                theta += STEPSIZE
 
-surface.write_to_png("image.png")  # Output to PNG
+        # Draw Text
+        x, y = _get_spiral_coords(theta)
+        ctx.move_to(x, y)
+        ctx.set_font_size(font_size)
+        r, g, b = _choose_colour()
+        ctx.set_source_rgb(r, g, b)
+        ctx.show_text(item)
+        # Add rectangle to rectangles
+        rectangles.append((x, x + extent.width, y - extent.height, y))
+
+    image_data = io.BytesIO()
+    surface.write_to_png(image_data)  # Output to PNG
+    image = Image.open(image_data)
+    callback_success(image)
+
+
+# region LOOKUP TABLES
+TIME_PERIODS = {
+    "All Time": pylast.PERIOD_OVERALL,
+    "Last Week": pylast.PERIOD_7DAYS,
+    "Last Month": pylast.PERIOD_1MONTH,
+    "Last 3 Months": pylast.PERIOD_3MONTHS,
+    "Last 6 months": pylast.PERIOD_6MONTHS,
+    "Last Year": pylast.PERIOD_12MONTHS,
+}
+
+TYPE_FUNCTIONS = {"Artist": _get_artists, "Album": _get_albums, "Song": _get_tracks}
+# endregion
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", root.destroy)
+    app = Application(master=root)
+    app.mainloop()
